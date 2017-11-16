d2f85ac035bf1df461504f1c4dce3cb204fc2adc
Mohit3003 <33267674+Mohit3003@users.noreply.github.com>
2017-11-17 02:40:55 +0530

    Add files via upload

--- google-vision/assets/main.js
+++ google-vision/assets/main.js
@@ -1,25 +1,16 @@
 $(function() {
   var client = ZAFClient.init();
-  client.invoke('resize', { width: '100%', height: '150px' });
+  client.invoke('resize', { width: '100%', height: '225px' });
   
   
   client.get('ticket.customField:custom_field_114101729171').then(
 	function(data) {		
-	if(data['ticket.customField:custom_field_114101729171']!=""){
-		var imageName = data['ticket.customField:custom_field_114101729171'];
-        showImageData(imageName,client);
+		if(data['ticket.customField:custom_field_114101729171']!=""){
+			var imageName = data['ticket.customField:custom_field_114101729171'];
+			showImageData(imageName,client);
+		}
 	}
-	}
-  );
-  
-  /*client.get('ticket.customField:custom_field_114101655912').then(
-	function(data) {
-		var imageName = data['ticket.customField:custom_field_114101655912'];
-		console.log(imageName);
-        showImageData(imageName,client);
-		//console.log("responseTwo = " +  responseTwo);
-	}
-  ); */ 
+  ); 
 });
 
 function showImageData(imageName, client){
@@ -47,31 +38,36 @@ function showImageData(imageName, client){
 	};
 	client.request(settings).then(
 		function(response) {
-			sendData(response);
+			sendData(response, imageName);
 		}
 	)
 }
 
-function sendData(data){
-		
-	var riskFactorOne="", descriptionOne="",riskFactorTwo="", descriptionTwo="";
+function sendData(data, imageName){		
+	var isHighRisk= false, isMediumRisk= false;isLowRisk= false;
 	
-	if(data.responses[0].webDetection.fullMatchingImages.length>0){
-		riskFactorOne="High";
-		descriptionOne = "Exact image has been found over the internet";
-	}else if(data.responses[0].webDetection.partialMatchingImages.length>0){
-		riskFactorOne="Medium";
-		descriptionOne = "A similiar image has been found over the internet";
-	}else{
-		riskFactorOne="Low";
-		descriptionOne = "This image has not been found over the internet";
+	if(data.responses[0].webDetection.fullMatchingImages!=null && data.responses[0].webDetection.fullMatchingImages !="undefined"){
+		if(data.responses[0].webDetection.fullMatchingImages.length>0){
+			isHighRisk = true;
+		}
 	}
-	
+	else if(data.responses[0].webDetection.partialMatchingImages!=null && data.responses[0].webDetection.partialMatchingImages !="undefined"){
+		if(data.responses[0].webDetection.partialMatchingImages.length>0){		
+			isMediumRisk=  true;
+		}
+	}else{
+		isLowRisk = true;
+	}
+
+	var imagePath = "https://storage.googleapis.com/artifacts-image/" + imageName;
 	
 	var requester_data = {
-		'riskFactorOne': riskFactorOne,
-		'descriptionOne': descriptionOne
-	};  
+		'imagePath' : imagePath,
+		'isHighRisk' :isHighRisk,
+		'isMediumRisk': isMediumRisk,
+		'isLowRisk' : isLowRisk		
+	};
+	
 	var source = $("#requester-template").html();
 	var template = Handlebars.compile(source);
 	var html = template(requester_data);
